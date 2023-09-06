95ecc2b5206fca85d4877ad6c27245a713b97dcb
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-09-07 01:10:52 +0700

    c

--- src/app/shared/layout/page/layout-admin/layout-admin.component.ts
+++ src/app/shared/layout/page/layout-admin/layout-admin.component.ts
@@ -3,6 +3,7 @@ import {
       faPenToSquare,
       faListCheck,
       faNewspaper,
+      faTags,
 } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
 
@@ -16,4 +17,78 @@ export class LayoutAdminComponent {
       faPenSquare = faPenToSquare;
       faListCheck = faListCheck;
       faNewspaper = faNewspaper;
+      faTags = faTags;
+      timeOut: any;
+      navItem = [
+            {
+                  title: 'Bài viết',
+                  opened: false,
+                  icon: 'description',
+                  sub: [
+                        {
+                              title: 'Tất cả bài viết',
+                              selected: false,
+                              url: 'admin/quan-ly-bai-viet',
+                        },
+                        {
+                              title: 'Viết bài viết mới',
+                              selected: false,
+                              url: 'admin/tao-bai-viet',
+                        },
+                        {
+                              title: 'Chuyên mục',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                  ],
+            },
+            {
+                  title: 'Trang',
+                  opened: false,
+                  icon: 'bookmark',
+                  sub: [
+                        {
+                              title: 'Trang chủ',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                        {
+                              title: 'Trang chuyên mục',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                  ],
+            },
+            {
+                  title: 'Người dùng',
+                  opened: false,
+                  icon: 'account_circle',
+                  sub: [
+                        {
+                              title: 'Tất cả người dùng',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                        {
+                              title: 'Hồ sơ người dùng',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                        {
+                              title: 'Hồ sơ',
+                              selected: false,
+                              url: 'admin/quan-ly-danh-muc',
+                        },
+                  ],
+            },
+      ];
+      hover(itemSelect: any) {
+            this.navItem.map((item: any) => {
+                  if (itemSelect.title === item.title) {
+                        item.opened = true;
+                  } else {
+                        item.opened = false;
+                  }
+            });
+      }
 }
